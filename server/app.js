225622873b7cc51c6c13d274c215457e96fc66ea
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-21 13:22:16 -0600

    basic mode done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+var treats = require('./routes/treats');
 var connectionString = "postgres://localhost:5432/sigma";
 
 /*** Build out a module to manage our treats requests. ***/
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// routes
+app.use('/treats', treats);
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./server/public/views/index.html'));
---
+++ server/routes/treats.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
+
+// request to get the treats from the database
+router.get('/', function(req, res) {
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM treats', function(err, result) {
+      done();
+
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+
+    });
+
+  });
+}); // get request ends
+
+// request to add a new task to the database
+router.post('/', function(req, res) {
+
+  var newTreat = req.body;
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query(
+      'INSERT INTO treats (name, description, pic) ' +
+      'VALUES ($1, $2, $3)',
+      [newTreat.name, newTreat.description, newTreat.url],
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.log('insert query error: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+
+  });
+
+}); // post request ends
+
+module.exports = router;
